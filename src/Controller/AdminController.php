dc992e9c4f5880f2c8df6eb9c817a29309be262c
YoLoN <shadyolown13@onet.pl>
2021-04-27 12:20:11 +0200

    improve markup and style, add dashboard section

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/susanoo/admin/susanoo')]
+#[Route('/susanoo')]
 
 class AdminController extends AbstractController
 {
