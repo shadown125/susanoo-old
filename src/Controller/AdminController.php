f093fdefeb51d4c5bae3b64f51aefac96d8b0efc
YoLoN <shadyolown13@onet.pl>
2021-04-29 21:53:33 +0200

    add functionality to register form

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 #[Route('/susanoo')]
 
@@ -17,8 +21,31 @@ class AdminController extends AbstractController
     }
 
     #[Route('/register', name: 'susanoo_register')]
-    public function register(): Response
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
-        return $this->render('admin/register.html.twig');
+        $user = new User;
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $user->setName($request->request->get('user')['name']);
+            $user->setLastName($request->request->get('user')['last_name']);
+            $user->setEmail($request->request->get('user')['email']);
+            $password = $passwordEncoder->encodePassword($user, $request->request->get('user')['password']['first']);
+            $user->setPassword($password);
+            $user->setRoles(['ROLE_USER']);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('susanoo_dashboard');
+        }
+
+        return $this->render('admin/register.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
